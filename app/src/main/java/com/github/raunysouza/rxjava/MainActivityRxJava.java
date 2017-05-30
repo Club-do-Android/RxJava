c264f6ab3861ec01e542cec9e882e9166d1afb45
Rauny Souza <rauny.souza@gmail.com>
2017-05-30 20:21:17 -0300

    Listing data

--- app/src/main/java/com/github/raunysouza/rxjava/MainActivityRxJava.java
+++ app/src/main/java/com/github/raunysouza/rxjava/MainActivityRxJava.java
@@ -3,8 +3,14 @@ package com.github.raunysouza.rxjava;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.raunysouza.rxjava.rest.ServiceRxJava;
@@ -12,6 +18,7 @@ import com.github.raunysouza.rxjava.rest.model.Character;
 import com.github.raunysouza.rxjava.rest.model.Planet;
 import com.github.raunysouza.rxjava.rest.model.Root;
 
+import java.util.Collections;
 import java.util.List;
 
 import io.reactivex.Observable;
@@ -26,10 +33,17 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class MainActivityRxJava extends AppCompatActivity {
 
+    private CharacterAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Load layout and setup any view
+        setContentView(R.layout.activity_main);
+
+        RecyclerView listRecyclerView = (RecyclerView) findViewById(R.id.list);
+        listRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        listRecyclerView.setHasFixedSize(true);
+        listRecyclerView.setAdapter(mAdapter = new CharacterAdapter());
 
         loadCharacters();
     }
@@ -50,8 +64,7 @@ public class MainActivityRxJava extends AppCompatActivity {
     }
 
     private void updateList(List<Pair<Character, Planet>> pairs) {
-        // Update list and refresh ui
-        Log.i("MainActivity", "Everything is loaded");
+        mAdapter.setItems(pairs);
     }
 
     private void showError(Throwable t) {
@@ -65,5 +78,44 @@ public class MainActivityRxJava extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
+    }
+
+    private class CharacterAdapter extends RecyclerView.Adapter<CharacterViewHolder> {
+
+        private List<Pair<Character, Planet>> mItems = Collections.emptyList();
+
+        @Override
+        public CharacterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new CharacterViewHolder(LayoutInflater.from(MainActivityRxJava.this).inflate(R.layout.list_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(CharacterViewHolder holder, int position) {
+            Pair<Character, Planet> characterPlanetPair = mItems.get(position);
+            holder.mNameTextView.setText(characterPlanetPair.first.getName());
+            holder.mPlanetTextView.setText(characterPlanetPair.second.getName());
+        }
+
+        @Override
+        public int getItemCount() {
+            return mItems.size();
+        }
+
+        public void setItems(List<Pair<Character, Planet>> items) {
+            mItems = items;
+            notifyDataSetChanged();
+        }
+    }
+
+    private class CharacterViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mNameTextView;
+        TextView mPlanetTextView;
+
+        public CharacterViewHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView) itemView.findViewById(R.id.name);
+            mPlanetTextView = (TextView) itemView.findViewById(R.id.planet);
+        }
     }
 }
